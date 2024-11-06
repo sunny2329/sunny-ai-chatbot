45d79b37a937d290da7ab481861dc62f5db8e1a3
Sunny <sobhit230@gmail.com>
2024-11-06 14:43:05 +0530

    Clerk and Prisma Installed

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+
+const isProtectedRoute = createRouteMatcher(['/chatbot'])
+
+export default clerkMiddleware(async (auth, req) => {
+    if (isProtectedRoute(req)) await auth.protect()
+})
+
+export const config = {
+    matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+}
